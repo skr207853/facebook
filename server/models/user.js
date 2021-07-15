044d9cff4221dd55b3c8d5545c03ba7fbecc4dd2
skr207853 <info.sumitraut@gmail.com>
2021-07-15 10:07:20 +0530

    adding feed where an user get all post from his friends with latest on top. and a feature where an user can add another friend as friend.

--- server/models/user.js
+++ server/models/user.js
@@ -2,36 +2,34 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema(
-  {
-    username: {
-      type: String,
-    },
-    email: {
-      type: String,
-    },
-    password: {
-      type: String,
-    },
-    friends: [
-      {
-        id : {
-          type: Schema.Types.ObjectId
-        }
-      }
-    ],
-    posts: [
-        {
-            title:String,
-            img:String,
-            date : {
-                type: Date,
-                default: Date.now
-            },
-            description:String,
-            likes:Number
-        }
-    ],
-  }
+    {
+        username: {
+            type: String,
+        },
+        email: {
+            type: String,
+        },
+        password: {
+            type: String,
+        },
+        friends: [
+            {
+                id: Schema.Types.ObjectId
+            }
+        ],
+        posts: [
+            {
+                title: String,
+                img: String,
+                date: {
+                    type: Date,
+                    default: Date.now
+                },
+                description: String,
+                likes: Number
+            }
+        ],
+    }
 );
 const User = mongoose.model("User", UserSchema);
 module.exports = User;

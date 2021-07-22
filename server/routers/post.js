e307168a38191f343603672fe5f70d55561d1c1d
Harsh Kumar <44499896+Harshkumar10@users.noreply.github.com>
2021-07-22 20:06:40 +0530

    cache implementation added
    node-cache npm is used to implement server side cache

--- server/routers/post.js
+++ server/routers/post.js
@@ -1,7 +1,10 @@
 const express = require("express");
-const {ObjectId} = require("mongodb");
+const { ObjectId } = require("mongodb");
 const router = new express.Router();
 const User = require("../models/user");
+const NodeCache = require("node-cache");
+const { rawListeners } = require("../models/user");
+const myCache = new NodeCache();
 
 router.post("/addpost/:id", async (req, res) => {
     try {
@@ -25,13 +28,16 @@ router.post("/addpost/:id", async (req, res) => {
     }
 });
 
-
 router.get("/getpost/:userid/:friends_post_id", async (req, res) => {
     try {
         const userFound = await User.findById(req.params.userid);
-        const fpi = userFound.friends_posts.find(el => el._id == req.params.friends_post_id);
+        const fpi = userFound.friends_posts.find(
+            (el) => el._id == req.params.friends_post_id
+        );
         const userFound2 = await User.findById(fpi.frd_id);
-        const thepost = userFound2.posts.find(el => el._id.toString() == fpi.post_id.toString());
+        const thepost = userFound2.posts.find(
+            (el) => el._id.toString() == fpi.post_id.toString()
+        );
         res.send(thepost);
     } catch (e) {
         res.send("error " + e);
@@ -43,7 +49,7 @@ router.post("/addfriend/:ids/:idf", async (req, res) => {
         const userFound = await User.findById(req.params.ids);
         const userFound2 = await User.findById(req.params.idf);
         if (!userFound || !userFound2) res.send("User not found");
-        if (await userFound.friends.some(el => el.id == req.params.idf)) {
+        if (await userFound.friends.some((el) => el.id == req.params.idf)) {
             res.send("He is already your friend.");
         }
         await userFound.friends.push({
@@ -56,34 +62,50 @@ router.post("/addfriend/:ids/:idf", async (req, res) => {
         await userFound2.save();
         {
             const post_array = [];
-            userFound.posts.forEach(el=> post_array.push({
-                    'frd_id': userFound._id,
-                    'post_id': post._id,
-                    'is_seen': false
-            }));
+            userFound.posts.forEach((el) =>
+                post_array.push({
+                    frd_id: userFound._id,
+                    post_id: post._id,
+                    is_seen: false,
+                })
+            );
             const feed_array = [];
             const feed_result_array = [];
-
         }
         userFound.save();
         res.send("say.. hi to your friend");
-
     } catch (err) {
         res.send("Error " + err);
     }
 });
-
+// 60f67942233cdf234c673f76
 router.get("/feed/:id", async (req, res) => {
     try {
         const userFound = await User.findById(req.params.id);
-        const feed_posts = [];
-        for (friend_post of userFound.friends_posts) {
-            if (friend_post.is_seen) continue;
-            // friend_post.is_seen = true;
-            userFound.save();
-            const userFound2 = await User.findById(friend_post.frd_id);
-            const thepost = userFound2.posts.find(el => el._id.toString() == friend_post.post_id.toString());
-            feed_posts.push(thepost);
+        let value = myCache.get(req.params.id);
+        let feed_posts = [];
+
+        if (value == undefined) {
+            // to handle miss
+            console.log("from the database");
+            for (friend_post of userFound.friends_posts) {
+                if (friend_post.is_seen) continue;
+                // friend_post.is_seen = true;
+                await userFound.save();
+                const userFound2 = await User.findById(friend_post.frd_id);
+                const thepost = userFound2.posts.find(
+                    (el) => el._id.toString() == friend_post.post_id.toString()
+                );
+                feed_posts.push(thepost);
+                // we have to push that data to the cache
+            }
+            const value = myCache.set(req.params.id, feed_posts, 10000);
+            console.log(value);
+        } else {
+            // we got the value
+            console.log(" from cache");
+            feed_posts = myCache.get(req.params.id);
+            console.log(feed_posts);
         }
         res.send(feed_posts);
     } catch (err) {
